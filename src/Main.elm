1831a69ff1a4f2383abfc2b1ab2dbce8c273fd5f
Nakta <hjlsgj@gmail.com>
2020-12-29 19:02:13 +0900

    항목 제목, 금액 추가 및 리스트 렌더링

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,12 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, h1, text)
-import Html.Events exposing (onClick)
+import Browser.Navigation as Nav
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Json.Decode as Decode
+import Url
 
 
 
@@ -11,20 +15,34 @@ import Html.Events exposing (onClick)
 
 main : Program () Model Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
 -- MODEL
 
 
+type alias Expense =
+    { title : String
+    , amount : Int
+    }
+
+
 type alias Model =
-    Int
+    { title : String
+    , amount : Int
+    , list : List Expense
+    }
 
 
-init : Model
-init =
-    0
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( Model "" 0 [], Cmd.none )
 
 
 
@@ -32,30 +50,98 @@ init =
 
 
 type Msg
-    = Increment
-    | Decrement
+    = HandleInputTitle String
+    | HandleInputAmount String
+    | HandleKeyDownInput Int
+    | HandleClickInsert
+    | None
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd msg )
 update msg model =
     case msg of
-        Increment ->
-            model + 1
+        HandleInputTitle title ->
+            ( { model | title = title }, Cmd.none )
 
-        Decrement ->
-            model - 1
+        HandleInputAmount value ->
+            case String.toInt value of
+                Just amount ->
+                    ( { model | amount = amount }, Cmd.none )
+
+                Nothing ->
+                    ( model, Cmd.none )
+
+        HandleKeyDownInput key ->
+            if key == 13 then
+                ( appendAndClearForm model, Cmd.none )
+
+            else
+                ( model, Cmd.none )
+
+        HandleClickInsert ->
+            ( appendAndClearForm model, Cmd.none )
+
+        None ->
+            ( model, Cmd.none )
+
+
+appendAndClearForm : Model -> Model
+appendAndClearForm model =
+    model |> appendForms |> clearForms
+
+
+appendForms : Model -> Model
+appendForms model =
+    { model | list = model.list ++ [ Expense model.title model.amount ] }
+
+
+clearForms : Model -> Model
+clearForms model =
+    { model | title = "", amount = 0 }
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub msg
+subscriptions _ =
+    Sub.none
 
 
 
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
+    { title = "Initial Page"
+    , body =
+        [ h1 []
+            [ text "📘 Housekeeping Book"
+            , small [ style "opacity" ".3", style "margin-left" ".5em" ] [ text "Save Money!!💵" ]
+            ]
+        , input [ type_ "text", onInput HandleInputTitle, placeholder "지출 항목 입력", value model.title ] []
+        , input [ type_ "number", onInput HandleInputAmount, onKeyDown HandleKeyDownInput, value (String.fromInt model.amount) ] []
+        , button [ type_ "button", onClick HandleClickInsert ] [ text "입력" ]
+        , div [] (renderLists model.list)
+        ]
+    }
+
+
+onKeyDown : (Int -> Msg) -> Attribute Msg
+onKeyDown tagger =
+    on "keydown" (Decode.map tagger keyCode)
+
+
+renderLists : List Expense -> List (Html msg)
+renderLists list =
+    List.map renderExpense list
+
+
+renderExpense : Expense -> Html msg
+renderExpense expense =
     div []
-        [ h1 [] [ text "Hello Elm!" ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Increment ] [ text "+" ]
-        , div [] [ text "Hello Elm" ]
+        [ h4 [] [ text expense.title ]
+        , div [] [ text (String.fromInt expense.amount) ]
         ]
